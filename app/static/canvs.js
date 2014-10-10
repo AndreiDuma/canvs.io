674aee3b805c3e84b2023ad26f4f4ad50add9181
Andrei Dorian Duma <andrei.duma.dorian@gmail.com>
2014-10-11 02:54:21 +0300

    Made resize.

--- app/static/canvs.js
+++ app/static/canvs.js
@@ -1,13 +1,33 @@
 $(document).ready(function() {
 
-//  $('canvas').width($(window).width());
-  //$('canvas').height($(window).height());
+  var canvas = $('canvas');
 
-  $('canvas').drawRect({
-    fillStyle: '#000',
-    x: 0, y: 0,
-    width: 100,
-    height: 100
+  var redraw = function() {
+    canvas.drawRect({
+      fillStyle: '#000',
+      x: 0, y: 0,
+      width: 100, height: 100
+    });
+
+  canvas.drawText({
+    fillStyle: 'red',
+    x: 100, y: 100,
+    fontSize: '50px',
+    text: 'Hello'
   });
+  };
+
+  var resizeCanvas = function() {
+    canvas.get(0).width = $(window).width();
+    canvas.get(0).height = $(window).height();
+    redraw();
+  };
+
+  var initialize = function() {
+    $(window).resize(resizeCanvas);
+    resizeCanvas();
+  };
+
+  initialize();
 
 });
