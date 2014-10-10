efd24e8aab1b23b1eab2b723ac7278eccaa47ab3
Andrei Dorian Duma <andrei.duma.dorian@gmail.com>
2014-10-11 02:28:43 +0300

    Yes.

--- app/static/canvs.js
+++ app/static/canvs.js
@@ -1,7 +1,13 @@
-var test = 2;
-$('canvas.body-canvas').drawRect({
-  fillStyle: '#fff',
-  x: 0, y: 100,
-  width: 100,
-  height: 100
+$(document).ready(function() {
+
+//  $('canvas').width($(window).width());
+  //$('canvas').height($(window).height());
+
+  $('canvas').drawRect({
+    fillStyle: '#000',
+    x: 0, y: 0,
+    width: 100,
+    height: 100
+  });
+
 });
