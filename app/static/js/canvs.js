dbc1558b7764db2fe19089c6c9f09eaaee22ecbd
Andrei Dorian Duma <andrei.duma.dorian@gmail.com>
2014-10-11 13:11:22 +0300

    Flying away from jcanvas

--- app/static/js/canvs.js
+++ app/static/js/canvs.js
@@ -2,34 +2,48 @@ $(document).ready(function() {
 
   var canvas = $("canvas");
 
-  $.get("/api/items", function(data) {
-    var items = data.items;
-
-    var textItems = items.text;
-    for (var i = 0, length = textItems.length; i < length; i++) {
-      var item = textItems[i];
-      canvas.drawText({
-        fillStyle: "#000",
-        fontSize: 25,
-        align: "left",
-        respectAlign: true,
-        draggable: true, 
-        x: item.x, y: item.y,
-        maxWidth: item.width,
-        text: item.text,
-      });
-    }
-
-    var imageItems = items.image;
-    for (var i = 0, length = imageItems.length; i < length; i++) {
-      var item = imageItems[i];
-      canvas.drawImage({
-        draggable: true, 
-        x: item.x, y: item.y,
-        source: item.data,
-      });
-    }
+  canvas.translateCanvas({
+    layer: true,
+    name: 'translate'
   });
+  canvas.setLayer('translate', {
+    draggable: true,
+    x: '+=1000'
+  });
+  canvas.drawLayers();
+
+  var loadItems = function() {
+    $.get("/api/items", function(data) {
+      var items = data.items;
+
+      /* Render text items */
+      var textItems = items.text;
+      for (var i = 0, length = textItems.length; i < length; i++) {
+        var item = textItems[i];
+        canvas.drawText({
+          fillStyle: "#000",
+          fontSize: 25,
+          align: "left",
+          respectAlign: true,
+          draggable: true, 
+          x: item.x, y: item.y,
+          maxWidth: item.width,
+          text: item.text,
+        });
+      }
+
+      /* Render images */
+      var imageItems = items.image;
+      for (var i = 0, length = imageItems.length; i < length; i++) {
+        var item = imageItems[i];
+        canvas.drawImage({
+          draggable: true, 
+          x: item.x, y: item.y,
+          source: item.data,
+        });
+      }
+    });
+  };
 
   var resizeCanvas = function() {
     canvas.get(0).width = $(window).width();
@@ -40,6 +54,7 @@ $(document).ready(function() {
   var initialize = function() {
     $(window).resize(resizeCanvas);
     resizeCanvas();
+    loadItems();
   };
 
   initialize();
