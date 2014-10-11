0c5a91cde299dc4d1319ecdeca9ec60cdbb54bae
Andrei Dorian Duma <andrei.duma.dorian@gmail.com>
2014-10-11 15:34:44 +0300

    Styled and added context menu js file

--- app/static/js/canvs.js
+++ app/static/js/canvs.js
@@ -49,9 +49,39 @@ $(document).ready(function() {
   initialize();
   */
 
-  $("body").scrollTop(50000);
-  $("body").scrollLeft(50000);
-
   $(".item").draggable({scroll: true});
   $("#items").draggable();
+
+  $("#content").scrollTop(50000);
+  $("#content").scrollLeft(50000);
+
+  $.get("/api/items", function(data) {
+    var items = data.items;
+
+    var textItems = items.text;
+    for (var i = 0, length = textItems.length; i < length; i++) {
+      var item = textItems[i];
+      canvas.drawText({
+        fillStyle: "#000",
+        fontSize: 25,
+        align: "left",
+        respectAlign: true,
+        draggable: true, 
+        x: item.x, y: item.y,
+        maxWidth: item.width,
+        text: item.text,
+      });
+    }
+
+    var imageItems = items.image;
+    for (var i = 0, length = imageItems.length; i < length; i++) {
+      var item = imageItems[i];
+      canvas.drawImage({
+        draggable: true, 
+        x: item.x, y: item.y,
+        source: item.data,
+      });
+    }
+  });
+
 });
