09d89e655962fa494715d451c48a88efad81031b
Calin Cruceru <calin@rosedu.org>
2014-10-11 11:07:19 +0300

    Add teardown function

--- utils/database.py
+++ utils/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from config import SQLALCHEMY_DATABASE_URI
+from app import app
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URI, convert_unicode=True)
@@ -15,3 +16,7 @@ Base.query = db_session.query_property()
 def init_db():
     import app.models
     Base.metadata.create_all(bind=engine)
+
+@app.teardown_appcontext
+def teardown_db(exception=None):
+    db_session.remove()
