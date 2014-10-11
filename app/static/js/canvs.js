2626ab3be863840183baf2101ef0045ddf2cd401
Andrei Dorian Duma <andrei.duma.dorian@gmail.com>
2014-10-11 23:15:34 +0300

    Added some logic.

--- app/static/js/canvs.js
+++ app/static/js/canvs.js
@@ -1,20 +1,62 @@
 $(document).ready(function() {
 
-  $("#items").draggable();
+  var itemsElem = $("#items");
+  itemsElem.draggable();
+
+  var createTextElem = function(textItem) {
+    var elem = $('<div class="item item-text"></div>'),
+        textElem = $('<div class="text"></div>'),
+        inputElem = $('<textarea></textarea>'),
+        okButton = $('<div class="ok-button"></div>'),
+        editButton = $('<div class="edit-button"></div>'),
+        deleteButton = $('<div class="delete-button"></div>');
+    elem.append(textElem, inputElem, okButton, editButton, deleteButton);
+    textElem.text(textItem.text);
+    elem.css({
+      left: textItem.x + "px",
+      top: textItem.y + "px",
+      width: textItem.width + "px",
+      height: textItem.height + "px"
+    });
+    elem.appendTo(itemsElem);
+    elem.draggable({scroll: true});
+    elem.resizable({minWidth: 100, minHeight: 100});
+
+    editButton.click(function() {
+      textElem.hide();
+      inputElem.val(textItem.text).show();
+      editButton.hide();
+      okButton.show().click(function() {
+        // TODO request to server
+        inputElem.hide();
+        textItem.text = inputElem.val();
+        textElem.text(textItem.text);
+        textElem.show();
+        okButton.hide();
+        editButton.show();
+      });
+    });
+
+    elem.find('.delete-button').click(function() {
+      // TODO request to server
+      elem.remove();
+    });
+  };
+
 
   setTimeout(function() {
-    $("body").scrollTop(50000);
-    $("body").scrollLeft(50000);
+    $("body").scrollTop(49900);
+    $("body").scrollLeft(49900);
   }, 0);
 
   $.get("/api/items-mock", function(data) {
-    var items = data.items,
-        itemsElem = $("#items");
+    var items = data.items;
 
     var textItems = items.text;
     for (var i = 0, length = textItems.length; i < length; i++) {
       var item = textItems[i];
 
+      /*
       var elem = $('<div class="item text"></div>');
       elem.html('<div class="inner">' + item.text + '</div>');
       elem.css({
@@ -32,7 +74,10 @@ $(document).ready(function() {
         el.html('<textarea></textarea>');
         el.find('textarea').text(text);
       });
+      */
+      createTextElem(item);
     }
+
 
     var imageItems = items.image;
     for (var i = 0, length = imageItems.length; i < length; i++) {
