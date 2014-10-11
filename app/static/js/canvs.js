09235524a72a4d115e5f7dd4c2ec98aa51294793
Andrei Dorian Duma <andrei.duma.dorian@gmail.com>
2014-10-11 12:17:24 +0300

    Fixed duplication.

--- app/static/js/canvs.js
+++ app/static/js/canvs.js
@@ -2,13 +2,9 @@ $(document).ready(function() {
 
   var canvas = $("canvas");
 
-  var items = {
-    text: [],
-    image: [],
-    link: []
-  };
+  $.get("/api/items", function(data) {
+    var items = data.items;
 
-  var redraw = function() {
     var textItems = items.text;
     for (var i = 0, length = textItems.length; i < length; i++) {
       var item = textItems[i];
@@ -33,17 +29,12 @@ $(document).ready(function() {
         source: item.data,
       });
     }
-  };
-
-  $.get("/api/items", function(data) {
-    items = data.items;
-    redraw();
   });
 
   var resizeCanvas = function() {
     canvas.get(0).width = $(window).width();
     canvas.get(0).height = $(window).height();
-    redraw();
+    canvas.drawLayers();
   };
 
   var initialize = function() {
