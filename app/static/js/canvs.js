7b9f08ca9e485784b49d068530d58727d6303271
Andrei Dorian Duma <andrei.duma.dorian@gmail.com>
2014-10-12 00:31:53 +0300

    Insignificant.

--- app/static/js/canvs.js
+++ app/static/js/canvs.js
@@ -76,48 +76,12 @@ $(document).ready(function() {
     var textItems = items.text;
     for (var i = 0, length = textItems.length; i < length; i++) {
       var item = textItems[i];
-
-      /*
-      var elem = $('<div class="item text"></div>');
-      elem.html('<div class="inner">' + item.text + '</div>');
-      elem.css({
-        left: item.x + "px",
-        top: item.y + "px",
-        width: item.width + "px",
-        height: item.height + "px"
-      });
-      elem.appendTo(itemsElem);
-      elem.draggable({scroll: true});
-      elem.resizable({minWidth: 100, minHeight: 100});
-      elem.dblclick(function(e) {
-        var el = $(e.target),
-            text = el.text();
-        el.html('<textarea></textarea>');
-        el.find('textarea').text(text);
-      });
-      */
       createTextElem(item);
     }
-
 
     var imageItems = items.image;
     for (var i = 0, length = imageItems.length; i < length; i++) {
       var item = imageItems[i];
-
-      /*
-      var elem = $('<div class="item image"></div>'),
-          imgElem = $('<img></img>');
-      imgElem.attr("src", item.data).appendTo(elem);
-      elem.css({
-        left: item.x + "px",
-        top: item.y + "px",
-        width: item.width + "px",
-        height: item.height + "px"
-      });
-      elem.appendTo(itemsElem);
-      elem.draggable({scroll: true});
-      elem.resizable({aspectRatio: true, minWidth: 100, minHeight: 100});
-      */
       createImageElem(item);
     }
   });
