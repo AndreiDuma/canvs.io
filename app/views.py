dfeed943066205b3e4611a048312e39bb4f9bf05
Calin Cruceru <calin@rosedu.org>
2014-10-12 05:13:17 +0300

    Return id when adding to/updating db

--- app/views.py
+++ app/views.py
@@ -9,20 +9,16 @@ from utils.database import db_session
 Auxiliar function used for adding a new entry in the texts table.
 """
 def add_text():
-    try:
-        x = request.form.get("x")
-        y = request.form.get("y")
-        width = request.form.get("width")
-        height = request.form.get("height")
-        text = request.form.get("text")
+    x = request.form.get("x")
+    y = request.form.get("y")
+    width = request.form.get("width")
+    height = request.form.get("height")
+    text = request.form.get("text")
 
-        new_text = Text(x, y, width, height, text)
-        db_session.add(new_text)
-        db_session.commit()
-        return Response("POST finished successfully", status=200)
-
-    except Exception as e:
-        return Response("Failure: " + e.message, status=400)
+    new_text = Text(x, y, width, height, text)
+    db_session.add(new_text)
+    db_session.commit()
+    return jsonify({'id' : new_text.id})
 
 """
 Auxiliar function used for updating an existing item from the texts table,
@@ -53,27 +49,22 @@ def update_text(id):
         Text.query.get(id).text = text
 
     db_session.commit()
-    return Response("PUT finished successfully", status=200)
+    return jsonify({'id' : id})
 
 """
 Auxiliar function used for adding a new entry in the images table.
 """
 def add_image():
-    try:
-        x = request.form.get("x")
-        y = request.form.get("y")
-        size = request.form.get("size")
-        url = request.form.get("url")
+    x = request.form.get("x")
+    y = request.form.get("y")
+    size = request.form.get("size")
+    url = request.form.get("url")
 
-        new_image = Image(x, y, size, url)
-        db_session.add(new_image)
-        db_session.commit()
+    new_image = Image(x, y, size, url)
+    db_session.add(new_image)
+    db_session.commit()
 
-        return Response(status=200)
-
-    except Exception as e:
-        print "Failure: " + e.message
-        return Response(status=400)
+    return jsonify({'id' : new_image.id})
 
 """
 Auxiliar function used for updating an existing item from the images table,
@@ -100,7 +91,7 @@ def update_image(id):
         Text.query.get(id).url = url
 
     db_session.commit()
-    return Response(status=200)
+    return jsonify({'id' : id})
 
 
 
